3145731e872a1aeec06813271942adc35c29f1da
Shawky Gamal <shawkygamal150@gmail.com>
2025-06-17 18:38:31 +0300

    adding the dotenv and make the main only read the routes

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+from routes import base
+
 
 app = FastAPI()
 
-@app.get("/welcome")
-def welcome():
-    return {"message": "Welcome to the FatAPI application!"}
+app.include_router(base.base_router)
+
+
