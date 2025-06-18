df4521ebd64b2dd208827cc2203dfe2da5cdf06a
Shawky Gamal <shawkygamal150@gmail.com>
2025-06-18 11:10:08 +0300

    makeing the config file for loading the .env content

---
+++ src/helpers/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    APP_NAME: str
+    APP_VERSION: str
+    OPENAI_API_KEY: str
+
+    class Config:
+        env_file = ".env"
+
+
+
+
+def get_settings() -> Settings:
+    return Settings()
