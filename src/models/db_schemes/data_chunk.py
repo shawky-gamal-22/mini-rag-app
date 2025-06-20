22c256ad5ff93a3e84c2b3674eee6ac7b8f1d1c8
Shawky Gamal <shawkygamal150@gmail.com>
2025-06-20 19:50:08 +0300

    schema for the data chunks in the mongo db

---
+++ src/models/db_schemes/data_chunk.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field, validator
+from bson.objectid import ObjectId
+from typing import Optional
+
+class DataChunk(BaseModel):
+        
+    _id: Optional[ObjectId]
+    chunk_text: str = Field(..., min_length=1)
+    chunk_meatadata: dict 
+    chunk_order: int = Field(..., gt=0)  # Ensures chunk_order is a positive integer
+    chunk_project_id: ObjectId
+
+
+
+    class Config:
+        arbitrary_types_allowed = True # Allows ObjectId to be used
+
+
+
+
+
+    
