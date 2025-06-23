f35924295b3f8af12667264da0ef1b35acedcf83
Shawky Gamal <shawkygamal150@gmail.com>
2025-06-23 14:45:26 +0300

    adding the storage of the asset collection in the upload end point to store the assets for any project

--- src/routes/data.py
+++ src/routes/data.py
@@ -10,7 +10,11 @@ import logging
 from .schemes.data import ProcessRequest
 from models.ProjectModel import ProjectModel
 from models.ChunkModel import ChunkModel
-from models.db_schemes import DataChunk
+from models.db_schemes import DataChunk, Asset
+from models.AssetModel import AssetModel
+from models.enums.AssetTypeEnum import AssetTypeEnum
+from bson import ObjectId
+
 
 logger = logging.getLogger("uvicorn.error")
 
@@ -63,11 +67,23 @@ async def upload_data(request:Request, project_id: str, file: UploadFile,
                 "message": ResponseSignal.FILE_UPLOAD_FAILED.value,
             }
         )
+    
+    # create an asset record in the database
+    asset_model = await AssetModel.create_instance(request.app.db_client)
+    asset_resource = Asset(
+        asset_project_id= project.id,
+        asset_type= AssetTypeEnum.FILE.value,
+        asset_name= file_id,
+        asset_size= os.path.getsize(file_path)
+    )
+
+    asset_record = await asset_model.create_asset(asset_resource)
+    logger.info(f"File {file.filename} uploaded successfully to {file_path}")
 
     return JSONResponse(
         content={
             "message": ResponseSignal.FILE_UPLOADED_SUCCESS.value,
-            "file_id": file_id
+            "file_id": str(asset_record.id)
         }
     )
 
