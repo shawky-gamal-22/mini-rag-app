6bfad18013cc98f2955814fdbc5923268b602aaf
Shawky Gamal <shawkygamal150@gmail.com>
2025-06-24 17:41:59 +0300

    returning the asset type with the asset project id, and make a function to return the one asset

--- src/models/AssetModel.py
+++ src/models/AssetModel.py
@@ -52,16 +52,31 @@ class AssetModel(BaseDataModel):
         asset.id = result.inserted_id
         return asset
 
-    async def get_all_project_assets(self, asset_project_id: str):
+    async def get_all_project_assets(self, asset_project_id: str, asset_type: str):
         """
         Get all assets for a specific project.
         
         :param project_id: The ID of the project to retrieve assets for.
         :return: A list of Asset objects associated with the project.
         """
-        cursor = self.collection.find({"asset_project_id": ObjectId(asset_project_id) if isinstance(asset_project_id, str) else asset_project_id})
+        cursor = self.collection.find({
+            "asset_project_id": ObjectId(asset_project_id) if isinstance(asset_project_id, str) else asset_project_id,
+            "asset_type": asset_type
+            })
         assets = []
         async for document in cursor:
             asset = Asset(**document)
             assets.append(asset)
         return assets
+
+    async def get_asset_record(self, asset_project_id:str, asset_name:str):
+
+        record = await self.collection.find_one({
+            "asset_project_id": ObjectId(asset_project_id) if isinstance(asset_project_id, str) else asset_project_id,
+            "asset_name": asset_name
+        })
+
+        if record:
+            return Asset(**record)
+        
+        return None
