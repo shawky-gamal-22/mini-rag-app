f339b7f166f09b6b1920b383deb49e43faa8dd52
Shawky Gamal <shawkygamal150@gmail.com>
2025-06-24 17:44:24 +0300

    enhance the endpoint that make if the user do not include any files the system should read all the assets related to the project and if the user send one asset we first check this asset exist and process it

--- src/routes/data.py
+++ src/routes/data.py
@@ -91,7 +91,7 @@ async def upload_data(request:Request, project_id: str, file: UploadFile,
 # it will be called after the file is uploaded successfully
 @data_router.post("/process/{project_id}")
 async def process_data(req:Request,project_id: str, request: ProcessRequest):
-    file_id = request.file_id
+    
     chunk_size = request.chunk_size
     overlap_size = request.overlap_size
     do_reset = request.do_reset
@@ -102,40 +102,47 @@ async def process_data(req:Request,project_id: str, request: ProcessRequest):
     project = await project_model.get_project_or_create_one(
         project_id=project_id
     )
-    
+    asset_model = await AssetModel.create_instance(req.app.db_client)
 
-    process_controller = ProcessController(project_id)
-    file_content = process_controller.get_file_content(file_id)
+    project_files_ids = {}
 
-    
-    file_chunks = process_controller.process_file_content(
-        file_content=file_content,
-        file_id=file_id,
-        chunk_size=chunk_size,
-        overlap=overlap_size
-    )
+    if request.file_id:
+        asset_record = await asset_model.get_asset_record(
+            asset_project_id=project.id,
+            asset_name=request.file_id
+        )
+        if asset_record is None:
+            return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={
+                    "signal": ResponseSignal.FILE_NOT_FOUND.value,
+                }
+            )
+        project_files_ids = {asset_record.id: asset_record.asset_name}
+    else:
+        # get all the assets for the project
+        
+        project_files = await asset_model.get_all_project_assets(
+            asset_project_id=project.id,
+            asset_type=AssetTypeEnum.FILE.value
+        )
 
-    
-    if file_chunks is None or len(file_chunks) == 0:
+        project_files_ids= {record.id:record.asset_name for record in project_files}
+
+    if len(project_files_ids) == 0:
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content={
-                "message": ResponseSignal.PROCESSING_FAILED.value,
+                "signal": ResponseSignal.NO_FILES_FOUND.value,
             }
         )
-    
-    # save the chunks to the database
-    file_chunks_records = [
-        DataChunk(
-            chunk_text = chunk.page_content,
-            chunk_meatadata= chunk.metadata,
-            chunk_order = i+1, 
-            chunk_project_id= project.id
-        )
-        for i, chunk in enumerate(file_chunks)
-        ]
-    chunk_model = await ChunkModel.create_instance(req.app.db_client)
 
+    process_controller = ProcessController(project_id)
+
+
+    no_of_recoreds = 0
+    no_files = 0
+    chunk_model = await ChunkModel.create_instance(req.app.db_client)
 
     if do_reset == 1 :
         # delete all the chunks for the project if do_reset is set to 1 and if the project exists
@@ -144,14 +151,54 @@ async def process_data(req:Request,project_id: str, request: ProcessRequest):
         )
         logger.info(f"Deleted {no_of_deleted_chunks} chunks for project {project_id}")
 
-    
-    no_of_recoreds = await chunk_model.insert_many_chunks(
-        chunks=file_chunks_records,
-    )
+    for asset_id, file_id in project_files_ids.items():
+        file_content = process_controller.get_file_content(file_id)
+
+        if file_content is None or len(file_content) == 0:
+            logger.error(f"File {file_id} is empty or not found.")
+            continue
+
+        file_chunks = process_controller.process_file_content(
+            file_content=file_content,
+            file_id=file_id,
+            chunk_size=chunk_size,
+            overlap=overlap_size
+        )
+
+        
+        if file_chunks is None or len(file_chunks) == 0:
+            return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={
+                    "message": ResponseSignal.PROCESSING_FAILED.value,
+                }
+            )
+        
+        # save the chunks to the database
+        file_chunks_records = [
+            DataChunk(
+                chunk_text = chunk.page_content,
+                chunk_meatadata= chunk.metadata,
+                chunk_order = i+1, 
+                chunk_project_id= project.id,
+                chunk_asset_id= asset_id
+            )
+            for i, chunk in enumerate(file_chunks)
+            ]
+
+
+        
+        
+        no_of_recoreds += await chunk_model.insert_many_chunks(
+            chunks=file_chunks_records,
+        )
+        no_files += 1
+
     return JSONResponse(
         content={
             "message": ResponseSignal.PROCESSING_SUCCESS.value,
-            "inserted_chunks": no_of_recoreds
+            "inserted_chunks": no_of_recoreds,
+            "processed_files": no_files,
         }
     )
 
