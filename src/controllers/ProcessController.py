ab6c5445e6fe0a9c9717df2748f0427182127576
Shawky Gamal <shawkygamal150@gmail.com>
2025-06-24 17:40:52 +0300

    checking that file exists in the system before loading it

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -23,6 +23,10 @@ class ProcessController(BaseController):
 
         file_ext = self.get_file_extension(file_id)
         file_path = os.path.join(self.project_path, file_id)
+        
+        if not os.path.exists(file_path):
+            return None
+        
         if file_ext == ProcessingEnum.TXT.value:
             return TextLoader(file_path, encoding='utf-8')
         
