03421e9f7c0caec44f189ce0fca9f1f9b3a3c974
Shawky Gamal <shawkygamal150@gmail.com>
2025-06-26 11:38:36 +0300

    makeing the cohere provider class but still not completed

---
+++ src/stores/llm/providers/CoHereProvider.py
@@ -0,0 +1,95 @@
+from ..LLMInterface import LLMInterface
+from ..LLMEnums import CohereEnum
+import cohere 
+import logging
+
+class CohereProvider(LLMInterface):
+
+    def __init__(self, api_key: str,
+                 default_input_max_characters: int=1000,
+                 default_output_max_characters: int=1000,
+                 default_generation_temperature: float=0.1):
+        
+        self.api_key = api_key
+        self.default_input_max_characters = default_input_max_characters
+        self.default_output_max_characters = default_output_max_characters
+        self.default_generation_temperature = default_generation_temperature
+        
+        self.generation_model_id = None
+        self.embedding_model_id = None
+        self.embedding_size = None
+
+        self.client = cohere.Client(
+            api_key=self.api_key
+        )
+        self.logger = logging.getLogger(__name__)
+
+    def set_generation_model(self, model_id: str):
+        """
+        Set the generation model to be used by the LLM.
+        
+        :param model_id: The identifier of the model to be set.
+        """
+        self.generation_model_id = model_id
+        self.logger.info(f"Generation model set to {model_id}")
+
+    def set_embedding_model(self, model_id: str, embedding_size: int):
+        """
+        Set the embedding model to be used by the LLM.
+        
+        :param model_id: The identifier of the embedding model to be set.
+        :param embedding_size: The size of the embedding.
+        """
+        self.embedding_model_id = model_id
+        self.embedding_size = embedding_size
+        self.logger.info(f"Embedding model set to {model_id} with size {embedding_size}")
+
+
+    def process_text(self, text: str) -> str:
+        """
+        Process the input text to ensure it meets the requirements of the LLM.
+        
+        :param text: The input text to be processed.
+        :return: The processed text.
+        """
+        if len(text) > self.default_input_max_characters:
+            self.logger.warning(f"Input text exceeds maximum length of {self.default_input_max_characters} characters.")
+            return text[:self.default_input_max_characters]
+        
+        return text.strip()
+    
+    def generate_text(self, prompt: str, chat_history: list=[], max_output_tokens: int=None,
+                      temperature: float= None) -> str:
+        if not self.client:
+            self.logger.error("CoHere client is not initialized.")
+            return None
+
+        if not self.generation_model_id:
+            self.logger.error("Generation model is not set.")
+            return None
+        
+        max_output_tokens = max_output_tokens if max_output_tokens is not None else self.default_output_max_characters
+        temperature = temperature if temperature is not None else self.default_generation_temperature
+
+        response = self.client.chat(
+            model = self.generation_model_id,
+            chat_history=chat_history,
+            message=self.process_text(prompt),
+            temperature= temperature,
+            max_tokens= max_output_tokens
+        )
+
+        if not response or not response.text:
+            self.logger.error("Error while generating text with cohere")
+            return None
+        
+        return response.text
+
+
+
+    
+    def construct_prompt(self, prompt: str, role: str):
+        return {
+            "role": role,
+            "text": self.process_text(prompt)
+        }
