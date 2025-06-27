18a92611bc3a3fba34e7c61e951239fa94dd1b48
Shawky Gamal <shawkygamal150@gmail.com>
2025-06-27 15:29:43 +0300

    editing the imports to fit the new paths

--- src/stores/llm/providers/OpenAIProvider.py
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -1,18 +1,18 @@
-from ...LLMInterface import LLMInterface
+from ..LLMInterface import LLMInterface
 from openai import OpenAI
 import logging
-from ...LLMEnums import OpenAIEnum
+from ..LLMEnums import OpenAIEnum
 
 class OpenAIProvider(LLMInterface):
 
     def __init__(self, api_key: str, api_url: str=None,
                  default_input_max_characters: int=1000,
-                 default_output_max_characters: int=1000,
+                 default_generation_max_output_tokens: int=1000,
                  default_generation_temperature: float=0.1):
         self.api_key = api_key
         self.api_url = api_url
         self.default_input_max_characters = default_input_max_characters
-        self.default_output_max_characters = default_output_max_characters
+        self.default_generation_max_output_tokens = default_generation_max_output_tokens
         self.default_generation_temperature = default_generation_temperature
         
         self.generation_model_id = None
@@ -47,7 +47,7 @@ class OpenAIProvider(LLMInterface):
             self.logger.error("Generation model is not set.")
             return None
         
-        max_output_tokens = max_output_tokens if max_output_tokens is not None else self.default_output_max_characters
+        max_output_tokens = max_output_tokens if max_output_tokens is not None else self.default_generation_max_output_tokens
         temperature = temperature if temperature is not None else self.default_generation_temperature
         chat_history.append(self.construct_prompt(prompt=prompt, role= OpenAIEnum.USER.value))
 
