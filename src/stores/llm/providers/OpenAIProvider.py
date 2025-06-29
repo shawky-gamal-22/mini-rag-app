2aa2ed41e23b385e79c55b94414119234b3e956b
Shawky Gamal <shawkygamal150@gmail.com>
2025-06-29 15:50:36 +0300

    editing the api_url parameter to base_url

--- src/stores/llm/providers/OpenAIProvider.py
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -20,7 +20,7 @@ class OpenAIProvider(LLMInterface):
         self.embedding_size = None
 
         self.client = OpenAI(api_key=self.api_key,
-                             api_url=self.api_url)
+                             base_url=self.api_url)
         
         self.logger = logging.getLogger(__name__)
 
