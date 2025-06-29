6e63dbb94d3053f26373915421244128b72308e7
Shawky Gamal <shawkygamal150@gmail.com>
2025-06-29 15:50:16 +0300

    adding some variables names

--- src/stores/llm/LLMProviderFactory.py
+++ src/stores/llm/LLMProviderFactory.py
@@ -1,5 +1,5 @@
 from .LLMEnums import LLMEnum
-from .providers import OpenAIProvider, CoHereProvider
+from .providers import OpenAIProvider, CohereProvider
 
 class LLMProviderFactory:
     def __init__(self, config: dict):
@@ -12,15 +12,15 @@ class LLMProviderFactory:
                 api_key= self.config.OPENAI_API_KEY,
                 api_url=self.config.OPENAI_API_URL,
                 default_input_max_characters= self.config.INPUT_DEFAULT_MAX_CHARACTERS,
-                default_output_max_characters= self.config.GENERATION_DEFAULT_MAX_TOKENS,
+                default_generation_max_output_tokens= self.config.GENERATION_DEFAULT_MAX_TOKENS,
                 default_generation_temperature= self.config.GENERATION_DEFAULT_TEMPERATURE
             )
 
         if provider == LLMEnum.COHERE.value:
-            return CoHereProvider(
+            return CohereProvider(
                 api_key= self.config.COHERE_API_KEY,
                 default_input_max_characters= self.config.INPUT_DEFAULT_MAX_CHARACTERS,
-                default_output_max_characters= self.config.GENERATION_DEFAULT_MAX_TOKENS,
+                default_generation_max_output_tokens= self.config.GENERATION_DEFAULT_MAX_TOKENS,
                 default_generation_temperature= self.config.GENERATION_DEFAULT_TEMPERATURE
             )
 
