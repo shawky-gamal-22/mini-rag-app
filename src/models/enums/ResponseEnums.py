bdae243f394256805115fe4832c96d31c648cc33
Shawky Gamal <shawkygamal150@gmail.com>
2025-07-01 16:10:37 +0300

    adding two new resonpse signals for error and success of the answering

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -18,3 +18,5 @@ class ResponseSignal(Enum):
     VECTORDB_COLLECTION_RETRIEVED = "vectordb_collection_retrieved"
     VECTORDB_SEARCH_ERROR = "vectordb_search_error"
     VECTORDB_SEARCH_SUCCESS= "vectordb_search_success"
+    RAG_ANSWER_SUCCESS= "rag_answer_succes"
+    RAG_ANSWER_ERROR = "rag_answer_errpr"
