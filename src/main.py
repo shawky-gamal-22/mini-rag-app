114237b5bf93c485359370248ae85369ad00b7a9
Shawky Gamal <shawkygamal150@gmail.com>
2025-08-01 20:46:58 +0300

    adding the middleware of the metrics in main

--- src/main.py
+++ src/main.py
@@ -7,7 +7,11 @@ from stores.llm.templates.template_parser import TemplateParser
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
+#Import metrics setup
+from utils import setup_metrics
+
 app = FastAPI()
+setup_metrics(app=app)
 
 @app.on_event("startup")
 async def startup_span():
