69d3b6ab6e031d010bf2e846f620970caee01420
Shawky Gamal <shawkygamal150@gmail.com>
2025-08-09 15:18:29 +0300

    adding github actions workflows and edit main route

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, APIRouter, Depends
 import os
 from helpers.config import get_settings, Settings
+from datetime import datetime
 
 
 base_router=APIRouter(
@@ -19,4 +20,5 @@ async def welcome(app_settings: Settings = Depends(get_settings)):
     return {
         "app_name": app_name,
         "app_version": app_version,
+        "datetime": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
