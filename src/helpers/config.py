45e1a755a6700feb2ba58b647d555e82db7af701
Shawky Gamal <shawkygamal150@gmail.com>
2025-09-18 15:31:08 +0300

    adding celery .env variables

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -45,6 +45,14 @@ class Settings(BaseSettings):
     PRIMARY_LANG:str = "en"
     DEFAULT_LANG: str= "en"
 
+    CELERY_BROKER_URL: str= None
+    CELERY_RESULT_BACKEND: str = None
+    CELERY_TASK_SERIALIZER: str = "json"
+    CELERY_TASK_TIME_LIMIT: int = 600  # 10 minutes
+    CELERY_TASK_ACKS_LATE: bool = True
+    CELERY_WORKER_CONCURRENCY: int =2
+    CELERY_FLOWER_PASSWORD:str
+
     class Config:
         env_file = ".env"
 
